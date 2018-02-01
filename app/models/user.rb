a9920fc7644a15cc75128e31fe594b65bce9a7bb
flanthedev <flanthedev@gmail.com>
2018-02-01 17:31:53 -0600

    create relation, add debugger

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-  # belongs_to :article
-
+  has_many :articles
+  before_save { self.email = email.downcase }
   validates :username,  presence: true, 
                         uniqueness: { case_sensitive: false },
                         length: { minumum: 3, maximum: 25 }
-  validates :password,  presence: true, 
+  validates :email,     presence: true, 
                         uniqueness: { case_sensitive: false },
                         length: { minumum: 5, maximum: 150 },
                         format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
