16af6f66cca6711689ee1119a432a2a2c0f2b719
flanthedev <flanthedev@gmail.com>
2018-02-01 17:01:52 -0600

    model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   # belongs_to :article
+
+  validates :username,  presence: true, 
+                        uniqueness: { case_sensitive: false },
+                        length: { minumum: 3, maximum: 25 }
+  validates :password,  presence: true, 
+                        uniqueness: { case_sensitive: false },
+                        length: { minumum: 5, maximum: 150 },
+                        format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
