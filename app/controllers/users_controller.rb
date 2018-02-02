76ae6104825d57eb9abf26c2c81482faa8181a7c
flanthedev <flanthedev@gmail.com>
2018-02-02 17:12:56 -0600

    add and edit users, form partials

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'Welcome to the Alpha Blog!'
+      redirect_to articles_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Profile has been update successfully."
+      redirect_to articles_path
+    else
+      render :edit
+
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+end
